e3379e5d9f6e8052118eada77db9b9fb0c1eae49
nurmhamadsoleh <nurmuhamadsoleh42@gmail.com>
2021-03-25 21:58:39 +0700

    update gateway

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Reseter Node express
 //import dependemcies yang di butuhkan 
 const express = require('express')
@@ -30,3 +31,33 @@ app.get("/" , (req, res)=>{
 
 // Listener / server yang di jalankan
 app.listen(3000 , ()=>{console.log("server yang berjalan di port 3000")})
+=======
+//import depedensi yang dibutuhkan
+const express = require('express');
+const cors = require('cors');
+const hb = require('express-handlebars');
+const app = express();
+const path = require('path');
+
+//set middleware
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname , "public")))
+
+//set view emgine
+app.set("views" , path.join(__dirname , "view"))
+app.set("view engine" , "handlebars")
+app.engine("handlebars" , hb({
+    layoutsDir : `${__dirname}/view/layouts`,
+    defaultLayout : "main_layouts",
+    partialsDir : `${__dirname}/view/components`
+}))
+
+//routing
+app.get("/" , (req, res)=>{
+    res.render("index" , {});
+});
+
+// listener
+app.listen(3000 , ()=>{console.log("server berjalan di port 3000")})
+>>>>>>> e8e8dc3c487e8e8f22374a02d16446d2fad0f33e
