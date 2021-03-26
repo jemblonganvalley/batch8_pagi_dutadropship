0a37d48d23917d3a06b2ffbb830da1c0eb99bcad
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-26 09:37:52 +0700

    update

--- server.js
+++ server.js
@@ -1,40 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Reseter Node express
-//import dependemcies yang di butuhkan 
-const express = require('express')
-const cors = require('cors')
-const hb = require('express-handlebars')
-const app = express()
-const path = require("path")
 
-// Set Middleware Membuat sebuah Function
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.join(__dirname, "public")))
-
-//Set view engine Memberikan tahu penempatan tools viwes (deklaras keseluruhan)
-app.set("views", path.join(__dirname, "view"))
-app.set("view engine" , "handlebars")
-app.engine("handlebars" , hb({
-    layoutsDir : `${__dirname}/view/layouts`,
-    // Componen HTMLTableRowElement, Footer terpisah
-    defaultLayout : "main_layout",
-    partialsDir : `${__dirname}/view/components`
-}))
-//Routing Ketika User akses sebuah file index
-app.get("/" , (req, res)=>{
-    res.render("index", {
-        title : "home page", 
-        text : "ini text dari server"
-    })
-})
-
-// Listener / server yang di jalankan
-app.listen(3000 , ()=>{console.log("server yang berjalan di port 3000")})
-=======
-=======
->>>>>>> origin
 //import depedensi yang dibutuhkan
 const express = require('express');
 const cors = require('cors');
@@ -61,10 +25,5 @@ app.get("/" , (req, res)=>{
     res.render("index" , {});
 });
 
-// listener
-<<<<<<< HEAD
+
 app.listen(3000 , ()=>{console.log("server berjalan di port 3000")})
->>>>>>> e8e8dc3c487e8e8f22374a02d16446d2fad0f33e
-=======
-app.listen(3000 , ()=>{console.log("server berjalan di port 3000")})
->>>>>>> origin
